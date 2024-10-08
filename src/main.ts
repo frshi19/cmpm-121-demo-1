db744a9c49828b8639ad1b584a0379fed1ff9dcb
Frank Shi <frshi@ucsc.edu>
2024-10-08 00:18:54 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,10 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button : HTMLButtonElement = document.createElement('button');
+button.innerHTML = '🥩';
+const appDiv: HTMLElement | null = document.getElementById('app');
+if (appDiv) {
+    appDiv.appendChild(button);
+}
