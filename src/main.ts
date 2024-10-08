bc143d939d671d6b3ae80b39ede41ee48c3655be
Frank Shi <frshi@ucsc.edu>
2024-10-08 00:54:16 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,39 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+let counter: number = 0;
+
+const counterDiv: HTMLDivElement = document.createElement('div');
+counterDiv.id = 'counterDisplay';
+updateCounterDisplay(counterDiv);
+
+// Append the counterDiv to the app element
+const appDiv: HTMLElement | null = document.getElementById('app');
+if (appDiv) {
+    appDiv.appendChild(counterDiv);
+}
+
+function updateCounterDisplay(div: HTMLDivElement) {
+  if (counter == 1) {
+    div.innerHTML = `${counter} steak`;
+  }
+  else {
+    div.innerHTML = `${counter} steaks`;
+  } 
+}
+
+function incrementCounter() {
+  counter += 1;
+  const counterDiv: HTMLElement | null = document.getElementById('counterDisplay');
+  if (counterDiv) {
+      updateCounterDisplay(counterDiv as HTMLDivElement);
+  }
+}
+
 const button: HTMLButtonElement = document.createElement("button");
 button.innerHTML = "🥩";
-const appDiv: HTMLElement | null = document.getElementById("app");
+button.onclick = incrementCounter;
 if (appDiv) {
   appDiv.appendChild(button);
 }
+
