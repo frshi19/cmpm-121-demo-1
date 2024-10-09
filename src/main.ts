290c2efcc696bfae4b5cb08e25d53b3d98d44aaa
Frank Shi <frshi@ucsc.edu>
2024-10-08 19:59:07 -0700

    Step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,13 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+// global variables
 let counter: number = 0;
 let lastTimestamp: number = performance.now();
 const incrementPerSecond: number = 1;
+let growthRate: number = 0;
 
+// create counter display
 const counterDiv: HTMLDivElement = document.createElement("div");
 counterDiv.id = "counterDisplay";
 updateCounterDisplay(counterDiv);
@@ -23,6 +26,7 @@ if (appDiv) {
   appDiv.appendChild(counterDiv);
 }
 
+// function for updating counter display
 function updateCounterDisplay(div: HTMLDivElement) {
   if (Math.floor(counter) == 1) {
     div.innerHTML = `${Math.floor(counter)} steak`;
@@ -31,6 +35,7 @@ function updateCounterDisplay(div: HTMLDivElement) {
   }
 }
 
+// Increases counter by 1
 function incrementCounter() {
   counter += 1;
   const counterDiv: HTMLElement | null =
@@ -40,6 +45,7 @@ function incrementCounter() {
   }
 }
 
+// create the button
 const button: HTMLButtonElement = document.createElement("button");
 button.innerHTML = "🥩";
 button.onclick = incrementCounter;
@@ -47,16 +53,48 @@ if (appDiv) {
   appDiv.appendChild(button);
 }
 
+// function to increate the growth rate
+function increaseGrowthRate() {
+  growthRate += 1;
+  counter -= 10;
+}
+
+// create upgrade div
+const upgradeDiv: HTMLDivElement = document.createElement("div");
+upgradeDiv.id = "upgrades";
+
+// Append the upgradeDiv to the app element
+if (appDiv) {
+  appDiv.appendChild(upgradeDiv);
+}
+
+// create upgrade button
+const upgradeButton: HTMLButtonElement = document.createElement("button");
+upgradeButton.innerHTML = "Butcher"
+if (appDiv) {
+  upgradeDiv.appendChild(upgradeButton);
+}
+upgradeButton.onclick = increaseGrowthRate;
+
+// Increments steak
 function continuousGrowth() {
   const currentTimestamp = performance.now();
   const delta = currentTimestamp - lastTimestamp; // Calculate time elapsed
   const increment = (incrementPerSecond * delta) / 1000; // Calculate frame-based increment
-  counter += increment;
+  counter += increment * growthRate;
 
   updateCounterDisplay(counterDiv); // Update counter display
 
   lastTimestamp = currentTimestamp;
   requestAnimationFrame(continuousGrowth); // Request next frame
+
+  // check if upgrade is available
+  if (counter < 10) {
+    upgradeButton.disabled = true;
+  }
+  else if (counter >= 10) {
+    upgradeButton.disabled = false;
+  }
 }
 
 requestAnimationFrame(continuousGrowth);
